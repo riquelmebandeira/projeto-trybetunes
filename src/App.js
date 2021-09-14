2b76a774d6b81458c348cfd66577845e4dad1673
Riquelme <riquelmebandeira1@outlook.com>
2021-09-14 16:16:22 -0300

    Requisito 1: concluido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+
+/**
+ * Usei esse video para descobrir como renderizar o componente NotFound para rotas não mapeadas.
+ * Link: shorturl.at/pqwNT
+ */
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
