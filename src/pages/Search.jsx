f2cae53651fcf4911fab0804f11e8f09d3c8a04b
Riquelme <riquelmebandeira1@outlook.com>
2021-09-15 15:30:13 -0300

    Requisito 5: concluido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      artistName: target.value,
+    });
+  }
+
   render() {
+    const { artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            value={ artistName }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ artistName.length < 2 }
+          >
+            Procurar
+          </button>
+        </form>
       </div>);
   }
 }
