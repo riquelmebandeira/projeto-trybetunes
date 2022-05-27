a238a1f937a5af3e1f5640685e9f0af4f4c9560c
riquelme <riquelmebandeira1@outlook.com>
2022-05-27 18:33:53 -0300

    feat: implementa a pagina de musicas favoritas

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      favorites: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setFavorites();
+  }
+
+  componentDidUpdate() {
+    this.setFavorites();
+  }
+
+  setFavorites = async () => {
+    this.setState({ favorites: await getFavoriteSongs() });
+  }
+
   render() {
+    const { favorites } = this.state;
+
     return (
       <div data-testid="page-favorites">
         <Header />
+        <section>
+          { favorites.map((trackInfo) => (
+            <MusicCard
+              key={ trackInfo.trackId }
+              trackInfo={ trackInfo }
+            />))}
+        </section>
       </div>);
   }
 }
