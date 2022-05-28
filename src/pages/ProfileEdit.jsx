c9fb67a74abe6732b4eb4d78c9eaa95ec61c598a
riquelme <riquelmebandeira1@outlook.com>
2022-05-27 21:09:52 -0300

    feat: implementa a pagina de ediçao de perfil

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,105 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Input from '../components/Input';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      image: '',
+      name: '',
+      email: '',
+      description: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setUser();
+  }
+
+  setUser = async () => {
+    const user = await getUser();
+    this.setState({ ...user });
+  }
+
+  handleChange = ({ target }) => {
+    const { id } = target;
+    const { value } = target;
+
+    this.setState({
+      [id]: value,
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await updateUser({ ...this.state });
+
+      window.location.href = 'http://localhost:3000/profile';
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
+    const { image, name, email, description } = this.state;
+    const EMAIL_REGEX = /.+@.+\..+/;
+
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        <main>
+          <form>
+            <Input
+              id="image"
+              value={ image }
+              onChange={ this.handleChange }
+              dataTestId="edit-input-image"
+            />
+            <Input
+              labelText="Nome"
+              id="name"
+              value={ name }
+              onChange={ this.handleChange }
+              dataTestId="edit-input-name"
+            />
+            <Input
+              labelText="Email"
+              id="email"
+              value={ email }
+              onChange={ this.handleChange }
+              dataTestId="edit-input-email"
+            />
+            <label htmlFor="description">
+              Descrição
+              <textarea
+                id="description"
+                value={ description }
+                onChange={ this.handleChange }
+                data-testid="edit-input-description"
+              />
+            </label>
+            <Link to="/profile">
+              <button
+                type="submit"
+                data-testid="edit-button-save"
+                disabled={
+                  !image
+                || !name
+                || !EMAIL_REGEX.test(email)
+                || !description
+                }
+                onClick={ (e) => this.handleSubmit(e) }
+              >
+                Salvar
+              </button>
+            </Link>
+          </form>
+        </main>
       </div>);
   }
 }
