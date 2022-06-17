141a2d5ae541e967c03141607aaf840cd4136803
riquelme <riquelmebandeira1@outlook.com>
2022-06-17 10:19:58 -0300

    refactor: separa o css do card de música num arquivo próprio

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
 import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import '../styles/components/MusicCard.css';
 
 class MusicCard extends React.Component {
   constructor() {
